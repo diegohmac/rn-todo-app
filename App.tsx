44ca2237c869916d5f55f2841275541a6bdc1175
Diego Machado <machadodiegoh@gmail.com>
2024-01-27 17:50:37 +0000

    initial setup

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { theme } from './src/styles/theme';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    Inter: require('./assets/fonts/Inter-Regular.ttf'),
+    'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ color: theme.colors.blueDark, fontFamily: "Inter", fontSize: theme.typography.large }}>HELLO </Text>
       <StatusBar style="auto" />
     </View>
   );
